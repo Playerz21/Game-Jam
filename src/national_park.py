e5f78aaa644df8f92839dbef1725f3aee7c46bee
Playerz21 <86472636+Playerz21@users.noreply.github.com>
2021-09-05 10:53:10 +0530

    Update national_park.py

--- src/national_park.py
+++ src/national_park.py
@@ -5,8 +5,8 @@ def main3():
     run=True
     while run:
         wn.blit(na_bg, (0, 0))
-        text=calibri.render("You reached the national park.",True,(0,255,255))
-        text2=calibri.render("Wohoo! You are safe from extinction",True,(0,255,255))
+        text=calibri.render("Wohoo! You reached the national",True,(0,255,255))
+        text2=calibri.render("park You are safe from extinction",True,(0,255,255))
         wn.blit(badge,(WIDTH/2-badge.get_width()/2,HEIGHT/2-badge.get_height()/2+100))
         wn.blit(text,(WIDTH/2-text.get_width()/2,100))
         wn.blit(text2,(WIDTH/2-text2.get_width()/2,100+text.get_height()))
